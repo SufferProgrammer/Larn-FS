f37baeb4c3b67a058378014020ad25edc7651555
SweetClown <pijipirma@gmail.com>
2016-05-30 13:05:18 +0700

    revision

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
+@app.route('/')
+def login():
     return render_template('index.html')
 
 if __name__=='__main__':
